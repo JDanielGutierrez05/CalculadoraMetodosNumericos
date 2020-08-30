caa8e04b92bdf496292f2a55e17e04a76d83ad49
Juan Daniel Gutierrez <jgutier@correo.tdea.edu.co>
2020-08-30 14:39:56 -0500

    WIp se crea proyecto inicial con HolaMundo

---
+++ src/com/metodosnumericos/Calculos.java
@@ -0,0 +1,8 @@
+package com.metodosnumericos;
+
+public class Calculos {
+
+    public void calcularDatos() {
+        System.out.println("Hola Mundo");
+    }
+}
---
+++ src/com/metodosnumericos/Main.java
@@ -0,0 +1,10 @@
+package com.metodosnumericos;
+
+public class Main {
+
+    public static void main(String[] args){
+        Calculos calculos = new Calculos();
+        calculos.calcularDatos();
+    }
+}
+
