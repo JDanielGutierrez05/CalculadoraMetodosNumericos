677df628b18fec090c47fdf0728af60922d999df
Juan Daniel Gutierrez <jgutier@correo.tdea.edu.co>
2020-09-09 21:13:14 -0500

    feat: se agregan librerias para derivacion

--- src/com/metodosnumericos/MetodoNewton.java
+++ src/com/metodosnumericos/MetodoNewton.java
@@ -1,8 +1,16 @@
 package com.metodosnumericos;
 
+import org.lsmp.djep.djep.DJep;
+import org.nfunk.jep.Node;
+import org.nfunk.jep.ParseException;
+
 import java.util.Scanner;
 
 public class MetodoNewton {
+
+    DJep djep;
+    Node nodoFuncion;
+    Node nodoDerivada;
 
     double limiteInferior;
     double limiteSuperior;
@@ -10,9 +18,14 @@ public class MetodoNewton {
     double xAnterior = Double.parseDouble("1000");
     double xActual;
     double error;
+    String funcionaDerivar;
 
     public void calcularDatos() {
-        System.out.println("Metodo Regla Falsa*****");
+        System.out.println("ingrese la funcion a derivar: ");
+        Scanner entradaEscaner = new Scanner(System.in);
+        derivar(entradaEscaner.next());
+
+      /*  System.out.println("Metodo Regla Falsa*****");
         System.out.println("Por favor introduzca el valor del limite inferior: ");
         Scanner entradaEscaner = new Scanner(System.in);
         limiteInferior = Double.parseDouble(entradaEscaner.next());
@@ -52,10 +65,54 @@ public class MetodoNewton {
             xActual = limiteInferior - (f(limiteInferior) * (limiteInferior - limiteSuperior) / (f(limiteInferior) - f(limiteSuperior)));
             iteraciones++;
         }
-        System.out.println("La raiz es: " + xActual + " y el numero de iteraciones fueron: " + iteraciones);
+        System.out.println("La raiz es: " + xActual + " y el numero de iteraciones fueron: " + iteraciones);*/
     }
 
     private double f(double valor) {
         return Math.sin(Math.pow(valor, 2) + 4);
+    }
+
+
+    public void derivar(String funcion) {
+        try {
+            this.djep = new DJep();
+            // agrega funciones estandares cos(x), sin(x)
+            this.djep.addStandardFunctions();
+
+            // agrega constantes estandares, pi, e, etc
+            this.djep.addStandardConstants();
+
+            // por si existe algun numero complejo
+            this.djep.addComplex();
+
+            // permite variables no declarables
+            this.djep.setAllowUndeclared(true);
+
+            // permite asignaciones
+            this.djep.setAllowAssignment(true);
+
+            // regla de multiplicacion o para sustraccion y sumas
+            this.djep.setImplicitMul(true);
+
+            // regla de multiplicacion o para sustraccion y sumas
+            this.djep.addStandardDiffRules();
+
+            // coloca el nodo con una funcion preestablecida
+            this.nodoFuncion = this.djep.parse(funcion);
+
+            // deriva la funcion con respecto a x
+            Node diff = this.djep.differentiate(nodoFuncion, "x");
+
+            // Simplificamos la funcion con respecto a x
+            this.nodoDerivada = this.djep.simplify(diff);
+
+            // Convertimos el valor simplificado en un String
+            funcion = this.djep.toString(this.nodoDerivada);
+            System.out.println("la derivada es: " + funcion);
+
+        } catch (ParseException e) {
+            funcion = "NaN";
+            System.out.println("Error: " + e.getErrorInfo());
+        }
     }
 }
