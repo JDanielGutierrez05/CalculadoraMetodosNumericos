f8bd67ac9af8dad93308cc3b2a0c451c4fc21206
Juan Daniel Gutierrez <jgutier@correo.tdea.edu.co>
2020-10-29 18:47:23 -0500

    WIP: parte basica del metodo

--- src/com/metodosnumericos/integracion/MetodoSimpson.java
+++ src/com/metodosnumericos/integracion/MetodoSimpson.java
@@ -1,8 +1,41 @@
 package com.metodosnumericos.integracion;
 
+import org.mariuszgromada.math.mxparser.Argument;
+import org.mariuszgromada.math.mxparser.Expression;
+
+import java.util.Scanner;
+
 public class MetodoSimpson {
 
+    double valorAIntervalo, valorBIntervalo, h;
+    String funcion;
+    int n;
+
     public void calcularDatos() {
-        System.out.println("Metodo pendiente por desarrollar");
+        System.out.println("*****Metodo de Simpsom*****");
+
+        System.out.println("Por favor ingrese la funcion: ");
+        Scanner entradaEscaner = new Scanner(System.in);
+        funcion = entradaEscaner.nextLine().toLowerCase().trim();
+        
+        System.out.println("Por favor introduzca el valor inicial del intervalo: ");
+        entradaEscaner = new Scanner(System.in);
+        valorAIntervalo = Double.parseDouble(entradaEscaner.next());
+
+        System.out.println("Por favor introduzca el valor final del intervalo: ");
+        entradaEscaner = new Scanner(System.in);
+        valorBIntervalo = Double.parseDouble(entradaEscaner.next());
+
+        System.out.println("Por favor introduzca el valor de n: ");
+        entradaEscaner = new Scanner(System.in);
+        n = Integer.parseInt(entradaEscaner.next());
+
+        h = (valorBIntervalo - valorAIntervalo) / n;
+
+    }
+
+    private double f(String funcion, double valor) {
+        Expression funcionResuelta = new Expression(funcion, new Argument("x = " + valor));
+        return funcionResuelta.calculate();
     }
 }
