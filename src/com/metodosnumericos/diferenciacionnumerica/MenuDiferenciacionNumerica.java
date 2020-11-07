dabf41f40cec8629cb9c8a75c19271377bc301ca
Juan Daniel Gutierrez <jgutier@correo.tdea.edu.co>
2020-11-06 20:49:37 -0500

    feat: se quita opcion innecesaria del menu

--- src/com/metodosnumericos/diferenciacionnumerica/MenuDiferenciacionNumerica.java
+++ src/com/metodosnumericos/diferenciacionnumerica/MenuDiferenciacionNumerica.java
@@ -8,18 +8,13 @@ public class MenuDiferenciacionNumerica {
 
     public void selecionarMetodoDifNumerica() {
         System.out.println("Seleccione el tipo de entrada");
-        System.out.println("1. Para ingresar la tabla de datos");
-        System.out.println("2. Para ingresar la formula ");
+        System.out.println("1. Para ingresar la formula ");
 
         Scanner entradaEscaner = new Scanner(System.in);
         int opcionSeleccionada = Integer.parseInt(entradaEscaner.next());
 
         switch (opcionSeleccionada) {
             case 1:
-                menuTipoDiferenciacion.selecionarTipoMetodoDifNumerica();
-                break;
-
-            case 2:
                 menuTipoDiferenciacion.selecionarTipoMetodoDifNumerica();
                 break;
 
